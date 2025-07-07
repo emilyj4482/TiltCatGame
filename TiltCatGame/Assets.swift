3093421a06454568980f06b650f7d08d2af83d7d
emilyj4482 <emilyj4482@gmail.com>
2025-07-08 00:14:10 +0900

    [chore] add image assets

---
+++ TiltCatGame/Assets.swift
@@ -0,0 +1,13 @@
+//
+//  Assets.swift
+//  TiltCatGame
+//
+//  Created by EMILY on 08/07/2025.
+//
+
+import Foundation
+
+enum Assets: String {
+    case background
+    case house
+}
---
+++ TiltCatGame/Cat.swift
@@ -0,0 +1,16 @@
+//
+//  Cat.swift
+//  TiltCatGame
+//
+//  Created by EMILY on 08/07/2025.
+//
+
+import Foundation
+
+enum Cat: String, CaseIterable {
+    case cheese
+    case mackerel
+    case gray
+    case vanila
+    case calico
+}
